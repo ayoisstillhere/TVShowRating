24274f09eae2061b4a6ca20e8ea2916a7699f65a
Ayodele Fagbami <afagbami@gmail.com>
2022-06-28 13:26:51 +0100

    added annotations

--- src/main/java/com/example/tvshowrating/controller/TvShowController.java
+++ src/main/java/com/example/tvshowrating/controller/TvShowController.java
@@ -1,4 +1,9 @@
 package com.example.tvshowrating.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "api/tvshows")
 public class TvShowController {
 }
