cc8a414e8fc4feabc64fb3014f2d9bedff9f0725
Ayodele Fagbami <afagbami@gmail.com>
2022-06-28 14:19:19 +0100

    create find by name in repository

--- src/main/java/com/example/tvshowrating/repository/TvShowRepository.java
+++ src/main/java/com/example/tvshowrating/repository/TvShowRepository.java
@@ -2,6 +2,12 @@ package com.example.tvshowrating.repository;
 
 import com.example.tvshowrating.model.TvShow;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
 
 public interface TvShowRepository extends JpaRepository<TvShow, Long> {
+
+    @Query("SELECT s FROM TvShow s WHERE s.name = ?1")
+    Optional<TvShow> findByName(String name);
 }
