4bf2b1ad890baca9de247ad1a3a2ab799e740f25
Ayodele Fagbami <afagbami@gmail.com>
2022-06-28 14:01:45 +0100

    Find all tvshows in database using repository instead of returning list

--- src/main/java/com/example/tvshowrating/service/TvShowService.java
+++ src/main/java/com/example/tvshowrating/service/TvShowService.java
@@ -1,6 +1,7 @@
 package com.example.tvshowrating.service;
 
 import com.example.tvshowrating.model.TvShow;
+import com.example.tvshowrating.repository.TvShowRepository;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
@@ -9,22 +10,13 @@ import java.util.List;
 
 @Service
 public class TvShowService {
+    private final TvShowRepository tvShowRepository;
+
+    public TvShowService(TvShowRepository tvShowRepository) {
+        this.tvShowRepository = tvShowRepository;
+    }
+
     public List<TvShow> getTvShows() {
-        return List.of(
-                new TvShow(
-                1L,
-                "Umbrella Academy",
-                "Netflix",
-                LocalDate.of(2019, Month.FEBRUARY,15),
-                5.0
-                ),
-                new TvShow(
-                        2L,
-                        "The Boys",
-                        "Amazon Prime",
-                        LocalDate.of(2019, Month.FEBRUARY,15),
-                        5.0
-                )
-        );
+        return tvShowRepository.findAll();
     }
 }
