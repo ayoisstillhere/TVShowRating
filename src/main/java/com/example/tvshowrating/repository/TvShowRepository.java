11b70c42382db2bd013a16d2e621a367d578019e
Ayodele Fagbami <afagbami@gmail.com>
2022-06-28 13:59:06 +0100

    extended jparepo

--- src/main/java/com/example/tvshowrating/repository/TvShowRepository.java
+++ src/main/java/com/example/tvshowrating/repository/TvShowRepository.java
@@ -1,4 +1,7 @@
 package com.example.tvshowrating.repository;
 
-public interface TvShowRepository {
+import com.example.tvshowrating.model.TvShow;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TvShowRepository extends JpaRepository<TvShow, Long> {
 }
