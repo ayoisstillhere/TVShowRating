a568a417d25378fd9643e385bffa524f94091025
Ayodele Fagbami <afagbami@gmail.com>
2022-06-28 14:43:33 +0100

    created updateTvshow in service

--- src/main/java/com/example/tvshowrating/service/TvShowService.java
+++ src/main/java/com/example/tvshowrating/service/TvShowService.java
@@ -4,9 +4,11 @@ import com.example.tvshowrating.model.TvShow;
 import com.example.tvshowrating.repository.TvShowRepository;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 @Service
@@ -32,8 +34,28 @@ public class TvShowService {
     public void deleteTvShow(Long id) {
         boolean exists = tvShowRepository.existsById(id);
         if (!exists) {
-            throw new IllegalStateException("tvshow with id " + " does not exist");
+            throw new IllegalStateException("tvshow with id " + id + " does not exist");
         }
         tvShowRepository.deleteById(id);
+    }
+
+    @Transactional
+    public void updateTvShow(Long id, String name, String network, Double rating) {
+        TvShow tvShow = tvShowRepository.findById(id)
+                .orElseThrow(() -> new IllegalStateException("tv show with id " + id + " doesn't exist"));
+        if(network != null && network.length() > 0 && !Objects.equals(tvShow.getNetwork(), network)) {
+            tvShow.setNetwork(network);
+        }
+        if(rating != null && !Objects.equals(tvShow.getRating(), rating)) {
+            tvShow.setRating(rating);
+        }
+
+        if (name != null && name.length() > 0 && !Objects.equals(tvShow.getName(), name)) {
+            Optional<TvShow> studentOptional = tvShowRepository.findByName(name);
+            if (studentOptional.isPresent()) {
+                throw new IllegalStateException("name taken");
+            }
+            tvShow.setName(name);
+        }
     }
 }
