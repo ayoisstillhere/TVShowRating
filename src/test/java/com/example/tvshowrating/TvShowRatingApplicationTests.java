c840d2efc141deb0c9dce29976890f178c0204c2
Ayodele Fagbami <afagbami@gmail.com>
2022-06-28 13:16:37 +0100

    Initial Commit

---
+++ src/test/java/com/example/tvshowrating/TvShowRatingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tvshowrating;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TvShowRatingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
