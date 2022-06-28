c1625854cb4069ec330dc895589ec3c7bc38c8a0
Ayodele Fagbami <afagbami@gmail.com>
2022-06-28 14:08:13 +0100

    Added dummy tvshow objects to database using config class

--- src/main/java/com/example/tvshowrating/config/TvShowConfig.java
+++ src/main/java/com/example/tvshowrating/config/TvShowConfig.java
@@ -1,4 +1,35 @@
 package com.example.tvshowrating.config;
 
+import com.example.tvshowrating.model.TvShow;
+import com.example.tvshowrating.repository.TvShowRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@Configuration
 public class TvShowConfig {
+    CommandLineRunner commandLineRunner(TvShowRepository repository) {
+
+        return args -> {
+           TvShow brelly =  new TvShow(
+                    1L,
+                    "Umbrella Academy",
+                    "Netflix",
+                    LocalDate.of(2019, Month.FEBRUARY, 15),
+                    5.0
+            );
+           TvShow boys = new TvShow(
+                   2L,
+                   "The Boys",
+                   "Amazon Prime",
+                   LocalDate.of(2019, Month.FEBRUARY, 15),
+                   5.0
+           );
+           repository.saveAll(List.of(brelly, boys));
+        };
+
+    }
 }
